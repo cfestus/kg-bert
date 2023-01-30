6a1c37ed3e49bbe07afd5216a3e45a856db28b8f
Chukwudi Festus <cfu.mdx@gmail.com>
2023-01-30 20:13:06 +0000

    Adding rdflib to the data preparation code

--- data/LED23/led.py
+++ data/LED23/led.py
@@ -1,75 +1,9 @@
-# python code that will iterate over led23.nt file and create the follwing files dev.tsv,
-# entities.txt, entity2text.txt, entity2txtlong.txt, relation2text.txt, relations.txt, text.tsv, train.tsv
-
-# import os
-
-
-# def write_to_file(file_path, data, mode='w'):
-#     with open(file_path, mode):
-#         f.write(data)
-
-
-# def get_entities_and_relations(nt_file):
-#     entities = set()
-#     relations = set()
-#     with open(nt_file, 'r'):
-#         for line in f:
-#             subj, rel, obj = line.strip().split()
-#             entities.add(subj)
-#             entities.add(obj)
-#             relations.add(rel)
-#     return entities, relations
-
-
-# def create_entity2text(entities, entity2text_file):
-#     with open(entity2text_file, 'w') as f:
-#         for entity in entities:
-#             f.write(f"{entity}\t{entity}\n")
-
-
-# def create_relation2text(relations, relation2text_file):
-#     with open(relation2text_file, 'w') as f:
-#         for rel in relations:
-#             f.write(f"{rel}\t{rel}\n")
-
-
-# def create_dev_train_text(nt_file, dev_file, train_file, text_file):
-#     with open(nt_file, 'r') as f:
-#         with open(dev_file, 'w') as dev:
-#             with open(train_file, 'w') as train:
-#                 with open(text_file, 'w') as text:
-#                     for i, line in enumerate(f):
-#                         subj, rel, obj = line.strip().split()
-#                         if i % 10 == 0:
-#                             dev.write(f"{subj}\t{rel}\t{obj}\n")
-#                         else:
-#                             train.write(f"{subj}\t{rel}\t{obj}\n")
-#                         text.write(f"{subj}\t{rel}\t{obj}\n")
-
-
-# if __name__ == '__main__':
-#     nt_file = "led23.nt"
-#     dev_file = "dev.tsv"
-#     train_file = "train.tsv"
-#     text_file = "text.tsv"
-#     entities_file = "entities.txt"
-#     entity2text_file = "entity2text.txt"
-#     entity2txtlong_file = "entity2txtlong.txt"
-#     relation2text_file = "relation2text.txt"
-#     relations_file = "relations.txt"
-
-#     entities, relations = get_entities_and_relations(nt_file)
-#     write_to_file(entities_file, "\n".join(entities))
-#     write_to_file(relations_file, "\n".join(relations))
-#     create_entity2text(entities, entity2text_file)
-#     write_to_file(entity2txtlong_file, "")
-#     create_relation2text(relations, relation2text_file)
-#     create_dev_train_text(nt_file, dev_file, train_file, text_file)
-
+# python code that will iterate over led23.nt file and create the files need for the KGC task
 
 import os
 from rdflib import Graph
-# See RDFLIB https://rdflib.readthedocs.io/en/stable/intro_to_parsing.html
+import html5lib
+
 
 def get_entities_and_relations(file):
     entities = set()
@@ -82,39 +16,30 @@ def get_entities_and_relations(file):
         relations.add(pred)
     return entities, relations
 
-# def get_entities_and_relations(nt_file):
-#     entities = set()
-#     relations = set()
-#     with open(nt_file, 'r') as f:
-#         for line in f:
-#             subject, relation, object_ = line.strip().split()
-#             entities.add(subject)
-#             entities.add(object_)
-#             relations.add(relation)
-#     return entities, relations
-
 
 def write_to_file(file_name, data):
-    with open(file_name, 'w') as f:
-        for line in data:
-            f.write(line + '\n')
+    if not data:
+        return
+    try:
+        with open(file_name, 'w') as f:
+            for line in data:
+                f.write(f"{line}\n")
+    except Exception as e:
+        print(f"Error writing to file {file_name}: {e}")
 
 
 def create_files(nt_file):
     entities, relations = get_entities_and_relations(nt_file)
-    entities = list(entities)
-    relations = list(relations)
-
-    write_to_file('entities.txt', entities)
-    write_to_file('relations.txt', relations)
-    write_to_file('entity2text.txt', entities)
-    write_to_file('entity2txtlong.txt', entities)
-    write_to_file('relation2text.txt', relations)
+    write_to_file('entities.txt', list(entities))
+    write_to_file('relations.txt', list(relations))
+    write_to_file('entity2text.txt', list(entities))
+    write_to_file('entity2txtlong.txt', list(entities))
+    write_to_file('relation2text.txt', list(relations))
 
     texts = []
     train_lines = []
     dev_lines = []
-    for i, entity in enumerate(entities):
+    for i, entity in enumerate(list(entities)):
         texts.append(f'{i}\t{entity}')
         if i % 10 == 0:
             dev_lines.append(f'{i}\t{entity}')
