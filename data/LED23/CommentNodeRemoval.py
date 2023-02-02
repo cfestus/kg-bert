2973ba9139b5b225c0130b7036df8141d3678f4e
Chukwudi Festus <cfu.mdx@gmail.com>
2023-02-02 10:30:50 +0000

    Removing led23 nt and comment node code

--- data/LED23/CommentNodeRemoval.py
+++
@@ -1,7 +0,0 @@
-with open('led23.nt', 'r') as file:
-    lines = file.readlines()
-
-with open('led23_without_comments.nt', 'w') as file:
-    for line in lines:
-        if '--' not in line:
-            file.write(line)
